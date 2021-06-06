fe3effcfc5ec9a81f203d73901cffc292b37732d
loveblue0121 <loveblue0121@gmail.com>
2021-06-06 10:47:31 +0800

    use dotenv

--- crawler/app_sql.js
+++ crawler/app_sql.js
@@ -3,12 +3,14 @@ const fs = require("fs/promises");
 const moment = require("moment");
 const mysql = require("mysql");
 const Promise = require("bluebird");
+require('dotenv').config();
 //資料庫連線
 let connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'licorne',
-    password : 'licorne0512',
-    database : 'stock'
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_PORT,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
   });
 
 connection = Promise.promisifyAll(connection);
