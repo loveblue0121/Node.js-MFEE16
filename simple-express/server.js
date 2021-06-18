7a4c276fe7431fd3b596dd4da70b20acbddac668
loveblue0121 <loveblue0121@gmail.com>
2021-06-18 15:11:30 +0800

    use express.static

--- simple-express/server.js
+++ simple-express/server.js
@@ -7,14 +7,18 @@ let app = express();
 req->router
 req->middleware.....->router
 */
+//app.use設定中間件
 app.use(function(req, res, next){
     let current = new Date();
     console.log(`有人來訪問了 在${current}`);
     //幾乎都要呼叫，讓他往下繼續
     next();
 });
+//可指定一個或多個目錄是「靜態資源目錄」
+//自動幫你為public裡面的檔案建立路由
+app.use(express.static("public")); //必須建立在所有中間件上面
 
-//路由router
+//路由router  (request, response){} 去回應這個請求
 //由上而下找，找到就停住了，不會在往下一個同樣的執行
 app.get("/",function(req, res){
     res.send("Hello Express");
