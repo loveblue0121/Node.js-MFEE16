8954a820f591d74f592a0fd4203c8d54050ad0e0
loveblue0121 <loveblue0121@gmail.com>
2021-06-18 16:13:17 +0800

    pug練習 用pug當 view engine

--- simple-express/server.js
+++ simple-express/server.js
@@ -17,17 +17,20 @@ app.use(function(req, res, next){
 //可指定一個或多個目錄是「靜態資源目錄」
 //自動幫你為public裡面的檔案建立路由
 app.use(express.static("public")); //必須建立在所有中間件上面
-
+//第一個是變數，第二個是檔案夾名稱
+app.set("views", "views");
+//告訴express我們用的view engine是pug
+app.set("view engine", "pug");
 //路由router  (request, response){} 去回應這個請求
 //由上而下找，找到就停住了，不會在往下一個同樣的執行
 app.get("/",function(req, res){
-    res.send("Hello Express");
+    res.render("index");
 });
 app.get("/test",function(req, res){
     res.send("TESTTTTTT");
 });
 app.get("/about",function(req, res){
-    res.send("about Express");
+    res.render("about");
 });
 app.listen(3000, () =>{
     console.log(`悄悄的我又來了~~~~ 在port3000`);
